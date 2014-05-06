779cde25f3c8406645e978c8aee59c83aab2a09a
Norniel <norniel@gmail.com>
2014-05-06 12:41:02 +0400

    removable wrapper, apple tree icons

---
+++ Game.Engine/Wrapers/RemovableWrapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Game.Engine.Objects;
+
+namespace Game.Engine.Wrapers
+{
+    public class RemovableWrapper<T> where T : GameObject
+    {
+        public GameObject GameObject { get; set; }
+
+        public Action<T> RemoveFromContainer { get; set; }
+    }
+}
