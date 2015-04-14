3ea1701ab08f279e2fe26119e8d78515fdb88d4d
Norniel <norniel@gmail.com>
2015-04-14 16:39:57 +0300

    LargeObjects base

---
+++ Game.Engine/Objects/LargeObjects/LargeObjectInner.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Game.Engine.Objects.LargeObjects
+{
+    class LargeObjectInner:FixedObject
+    {
+        private readonly Lazy<List<LargeObjectOuter>> _lazyObjectsOuter;
+
+        public LargeObjectInner()
+        {
+            _lazyObjectsOuter = new Lazy<List<LargeObjectOuter>>(InitOuterObjects, true);
+        }
+  
+        protected virtual List<LargeObjectOuter> InitOuterObjects()
+        {
+            var result = new List<LargeObjectOuter>();
+            for (int i = 0; i < this.Size.Width; i++)
+            {
+                for (int j = 0; j < this.Size.Height; j++)
+                {
+                    var largeObjectOuter = new LargeObjectOuter(this, new Point(i, j), false);
+                    result.Add(largeObjectOuter);
+                }                
+            }
+            return result;
+        }      
+
+        public IEnumerable<LargeObjectOuterAbstract> OuterObjects {
+            get { return _lazyObjectsOuter.Value; }
+        }
+
+        protected class LargeObjectOuter : LargeObjectOuterAbstract
+        {
+            public LargeObjectOuter(LargeObjectInner largeObjectInner, Point placeInObject, bool isTransparent) : base(largeObjectInner, placeInObject, isTransparent)
+            {
+            }
+        }
+    }
+}
---
+++ Game.Engine/Objects/LargeObjects/LargeObjectOuter.cs
@@ -0,0 +1,29 @@
+﻿namespace Game.Engine.Objects.LargeObjects
+{
+    abstract class LargeObjectOuterAbstract : FixedObject
+    {
+        private readonly LargeObjectInner _innerObject;
+        private readonly Point _placeInObject;
+
+        public bool IsTransparent { get; private set; }
+
+        public LargeObjectOuterAbstract(LargeObjectInner largeObjectInner, Point placeInObject, bool isTransparent)
+        {
+            _innerObject = largeObjectInner;
+            _placeInObject = placeInObject;
+            IsTransparent = IsTransparent;
+            IsPassable = false;
+            Size = new Size(1, 1);
+        }
+
+        public override string Name
+        {
+            get { return _innerObject.Name; }
+        }
+
+        public override uint GetDrawingCode()
+        {
+            return _innerObject.Id;
+        }
+    }
+}
